de3a030f811bb8399ffe3d2be35f129ad9db6f06
Michu2005 <137965415+Michu2005@users.noreply.github.com>
2023-07-14 13:56:31 -0500

    Add files via upload
    Separación de capas y validación de datos en FOMS

--- Program.cs
+++ Program.cs
@@ -1,36 +1,17 @@
-﻿//// Tipos de datos
-//int myint = 5;
-//double mydouble = 3.4;
-//float myfloat = 4.56f;
-//decimal mydecimal = 4.56m;
-
-//string mystring = "Michelle";
-
-//bool mybool = true;
-
-//var myVar = 23.4;
-
-
-//Console.WriteLine($"Mi valor entero es..: {myint, 20:N2}");
-//Console.WriteLine($"Mi valor doble es..: {mydouble, 20:N2}");
-//Console.WriteLine($"Mi valor float es..: {myfloat, 20:P2}");
-//Console.WriteLine($"Mi valor decimal es: {mydecimal, 20:C2}");
-//Console.WriteLine($"Mi nombre es.......: {mystring}");
-//Console.WriteLine($"Mi boolean es......: {(mybool ? "Correcto": "Incorrecto")}");  //if en una sola linea o tenrnarios
-
-
-using OOPConceptos.Logica;
-try
+namespace OOPConceptos.Interfaz
 {
-    var fecha1 = new Date()
+    internal static class Program
     {
-        Yaer = 2023,
-        Month = 2,
-        Day = 28
-    };
-    Console.WriteLine(fecha1);
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Error: {ex.Message}");
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
 }
